5025f531d63fd678ffba5cede7ab2d0a9230240f
Nicholas Papenburg <nicpapenburg@gmail.com>
2024-05-30 15:32:01 -0700

    Airflow and other tools are now running via docker

---
+++ script/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+# Upgrade pip and install requirements if the requirements file exists
+if [ -e "/opt/airflow/requirements.txt" ]; then
+  python -m pip install --upgrade pip
+  pip install --user -r /opt/airflow/requirements.txt
+fi
+
+# Initialize the database and create an admin user if it hasn't been done yet
+if [ ! -f "/opt/airflow/airflow.db" ]; then
+  airflow db init
+  airflow users create \
+    --username admin \
+    --firstname admin \
+    --lastname admin \
+    --role Admin \
+    --email admin@example.com \
+    --password admin
+  touch /opt/airflow/airflow.db
+fi
+
+# Upgrade the database
+airflow db upgrade
+
+# Start the Airflow webserver
+exec airflow webserver
